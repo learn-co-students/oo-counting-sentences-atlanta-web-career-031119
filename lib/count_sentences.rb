97801126546ab4f9e62a58cbd3096a8a483284cf
Ansara Hollinger <ansara.hooks@gmail.com>
2019-03-13 16:38:20 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,19 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    my_array = self.split(/[?.!!]/) - [""]
+    my_array.count
   end
 end
